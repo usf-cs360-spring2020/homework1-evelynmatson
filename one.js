d432d4430349af742085553bc9c281d1eefe6d6a
Eve Matson <evelyn@Eves-MacBook-Pro.local>
2020-02-06 16:26:16 -0800

    working on dates

--- one.js
+++ one.js
@@ -6,10 +6,16 @@ let loadDataOne = function() {
     let csv = d3.csv("1 2018 enplaned per month by region summary.csv").then(function(data) {
       for (element of data) {
         console.log(element);
+        console.log(convertActivityPeriod(element["Activity Period"]));
       }
 
     });
     console.log(csv);
+}
+
+let convertActivityPeriod = function(monthstring) {
+    return new Date(monthstring.substring(0, 5), monthstring.substring(5,7), 1); //YYYY, MM, DD
+    // TODO FIX THIS
 }
 
 loadDataOne();
