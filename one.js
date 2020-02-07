b68f81422add892bfa5d6657e24f8c6ae8429bcc
evelynmatson <ematson@dons.usfca.edu>
2020-02-07 15:45:15 -0800

    This really is javascript

--- one.js
+++ one.js
@@ -1,21 +1,43 @@
 /*
+ * This function converts date values during csv import
+ */
+let convertRow = function(row, index) {
+    out = {};
+
+    out["month"] = convertActivityPeriod(row["Activity Period"]);
+    out["passenger count"] = row["Passenger Count"];
+    out["geo"] = row["GEO Summary"];
+
+    return out;
+}
+
+/*
  * This function will load the csv from disk for the 1st visualization
- *
  */
 let loadDataOne = function() {
-    let csv = d3.csv("1 2018 enplaned per month by region summary.csv").then(function(data) {
-      for (element of data) {
-        console.log(element);
-        console.log(convertActivityPeriod(element["Activity Period"]));
-      }
+    let csv = d3.csv("1 2018 enplaned per month by region summary.csv", convertRow).then(function(data) {
+        rows = [];
+        for (let element of data) {
+            row = {};
 
+            row["month"] = convertActivityPeriod(element["Activity Period"]);
+            row["passenger count"] = element["Passenger Count"];
+            row["geo"] = element["GEO Summary"];
+             // console.log(row);
+            rows.
+      }
     });
+
     console.log(csv);
 }
 
 let convertActivityPeriod = function(monthstring) {
-    return new Date(monthstring.substring(0, 5), monthstring.substring(5,7), 1); //YYYY, MM, DD
-    // TODO FIX THIS
+    let parseDate = d3.timeParse('%Y%m');
+    let date = parseDate(monthstring);
+
+    return date;
+    // console.log(date);
+
 }
 
 loadDataOne();
