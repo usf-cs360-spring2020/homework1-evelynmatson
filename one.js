279cd0c63e7c8b196826ed779e5cff07fbcf2e31
evelynmatson <ematson@dons.usfca.edu>
2020-02-08 20:19:16 -0800

    axes getting close

--- one.js
+++ one.js
@@ -10,22 +10,6 @@ let axes;
 let plot;
 
 /**
- * This function converts date values during csv import
- * @param row the row object to convert
- * @param index who knows! I think this is required
- * @returns the converted row
- */
-let convertRow = function(row, index) {
-    let out = {};
-
-    out["month"] = convertActivityPeriod(row["Activity Period"]);
-    out["geo"] = row["GEO Region"];
-    out["passengers"] = parseInt(row["Passenger Count"]);
-
-    return out;
-};
-
-/**
  * This function will draw the first visualization.
  */
 let visualizationOne = function() {
@@ -37,7 +21,7 @@ let visualizationOne = function() {
     // svg margins
     config.margin.top = 50;
     config.margin.right = 10;
-    config.margin.bottom = 10;
+    config.margin.bottom = 40;
     config.margin.left = 70;
 
     // Plot specs
@@ -72,15 +56,16 @@ let visualizationOne = function() {
 
     // Make some scales!
     // Month scale (y)
-    console.log("height is :", config.plot.height);
+    // console.log("height is :", config.plot.height);
     scales.month = d3.scaleBand()
         .rangeRound([0, config.plot.height])
         .paddingInner(config.plot.paddingBetweenMonths);
 
-    scales.passengers = d3.scaleLinear();
+    scales.passengers = d3.scaleLinear()
+        .nice();
         // Will give a range later, when we know more about the data
 
-    console.log("width is :", config.plot.width);
+    // console.log("width is :", config.plot.width);
     scales.regions = d3.scaleBand()
         .rangeRound([0, config.plot.width])
         .paddingInner(config.plot.paddingBetweenRegions);
@@ -117,7 +102,7 @@ let drawOne = function(data) {
         .map(row => row['month'])
         .sort(function(a,b) {return a - b;});
     scales.month.domain(dates);
-    console.log("Months bandwidth is :", scales.month.bandwidth());
+    // console.log("Months bandwidth is :", scales.month.bandwidth());
 
     let regions = data
         .sort(function(a, b) {
@@ -125,16 +110,17 @@ let drawOne = function(data) {
         })
         .map(row => row['geo'])
         .unique();
-
     console.log(regions);
+
+    // console.log(regions);
     scales.regions.domain(regions);
-    console.log("Regions bandwidth is :", scales.regions.bandwidth());
+    // console.log("Regions bandwidth is :", scales.regions.bandwidth());
 
 
     let maxPassengers = Math.max(... data.map(row => row['passengers']));
     scales.passengers.domain([0,maxPassengers])
         .range([0, scales.regions.bandwidth()]);
-    console.log("Max passengers is :", maxPassengers);
+    // console.log("Max passengers is :", maxPassengers);
 
     // TODO actually draw axes
 
@@ -156,23 +142,36 @@ let drawOne = function(data) {
 
     // Finally, set up axes
     let monthsAxis = d3.axisLeft(scales.month)
-        .tickPadding(0)
+        // .tickPadding(0)
         .tickFormat(monthsFormatter);
     axes.months = monthsAxis;
-
     let monthsAxisGroup = plot.append("g")
         .attr("id", "months-axis")
         .attr("class", "axis");
     monthsAxisGroup.call(monthsAxis);
 
     let regionsAxis = d3.axisTop(scales.regions)
-        .tickPadding(0);
-    axes.regions = regionsAxis;
+        // .tickPadding(0);
 
+    axes.regions = regionsAxis;
     let regionsAxisGroup = plot.append("g")
         .attr("id", "regions-axis")
         .attr("class", "axis");
     regionsAxisGroup.call(regionsAxis);
+
+    let passengersAxis = d3.axisBottom(scales.passengers)
+        // .tickPadding(0)
+        .tickFormat(passengerTicksFormatter)
+        .ticks(3);
+    axes.passengers = passengersAxis;
+    for( let [index, region] of regions.entries() ) {
+        let passengersAxisGroup = plot.append("g")
+            .attr("id", "regions-axis")
+            .attr("class", "axis" + index.toString())
+            .attr("transform", translate(scales.regions(region) ,config.plot.height));
+        passengersAxisGroup.call(passengersAxis);
+    }
+
 };
 
 /**
@@ -190,6 +189,15 @@ function monthsFormatter(d) {
         "July", "August", "September", "October", "November", "December"
     ];  // From https://stackoverflow.com/questions/1643320/get-month-name-from-date
     return monthNames[d.getMonth()];
+}
+
+/*
+ * Helps format the ticks for the passenger count axes.
+ */
+function passengerTicksFormatter(d) {
+    if(d === 0 ) {return ""}
+    // if(d % 50000 !== 0) {return ""}
+    return (d/1000).toString() + "k"
 }
 
 /**
@@ -213,6 +221,23 @@ function convertActivityPeriod(monthstring) {
     return parseDate(monthstring);
     // console.log(date);
 }
+
+/**
+ * This function converts date values during csv import
+ * @param row the row object to convert
+ * @param index who knows! I think this is required
+ * @returns the converted row
+ */
+let convertRow = function(row, index) {
+    let out = {};
+
+    out["month"] = convertActivityPeriod(row["Activity Period"]);
+    out["geo"] = row["GEO Region"];
+    out["passengers"] = parseInt(row["Passenger Count"]);
+
+    return out;
+};
+
 
 /*
  * Finds the largest passenger count for any month for region in data entry a
